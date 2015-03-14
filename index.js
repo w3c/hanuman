68a366a42f02bca5337d0331af039ef5ac0caf58
plehegar <plh@w3.org>
2015-03-14 10:33:34 -0400

    Switched to express 0.4.x

--- index.js
+++ index.js
@@ -11,27 +11,27 @@
 
  [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
 */
-
 var express = require("express"),
     gh = require("simple-github")({
         owner: "w3c",
         repo: "web-platform-tests"
-    });
+    }),
+    compression = require('compression');
 
 var server = express();
 
 var issues;
 
 function updateIssues() {
-     gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=open").then(function (opens) {
-      gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=closed").then(function(closed) {
-        var new_issues = closed.concat(opens);
-        new_issues.sort(function(a,b){
-            return a.number - b.number;
-        });
-        issues = new_issues;
-        console.log("Updated with", issues.length + " github objects");
+  gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=open").then(function (opens) {
+    gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=closed").then(function(closed) {
+      var new_issues = closed.concat(opens);
+      new_issues.sort(function(a,b){
+        return a.number - b.number;
       });
+      issues = new_issues;
+      console.log("Updated with", issues.length + " github objects");
+    });
   });
 }
 
@@ -39,28 +39,25 @@ setInterval(updateIssues, 3610000); // update every hour or so
 updateIssues();
 
 // Configuration
-server.configure(function(){
-    server.use(function (req, res, next) {
-        res.set("Access-Control-Allow-Origin", "*");
-        next();
-    });
-    server.use(express.compress());
-    server.use(server.router);
+server.use(function (req, res, next) {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
 });
+server.use(compression());
 
-server.configure('development', function() {
-    server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-server.configure('production', function() {
-    server.use(express.errorHandler());
-});
 
 server.get('/issues', function (req, res, next) {
     res.jsonp(issues);
 });
 
-var port = process.env.PORT || 8000;
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+server.use(errorHandler);
+
+var port = process.env.PORT || 8080;
 server.listen(port, function() {
     console.log("Express server listening on port %d in %s mode", port, server.settings.env);
 });
