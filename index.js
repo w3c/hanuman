7838a098a19051631b14d8ff28d3963a5beb133d
plehegar <plh@w3.org>
2015-03-14 16:50:23 -0400

    Modularization and added a few middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*
- Copyright © 2014 World Wide Web Consortium, (Massachusetts Institute of Technology,
+ Copyright © 2015 World Wide Web Consortium, (Massachusetts Institute of Technology,
  European Research Consortium for Informatics and Mathematics, Keio University, Beihang).
  All Rights Reserved.
 
@@ -12,52 +12,36 @@
  [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
 */
 var express = require("express"),
+    issues = require("./issues"),
     gh = require("simple-github")({
         owner: "w3c",
         repo: "web-platform-tests"
     }),
-    compression = require('compression');
+    compression = require('compression'),
+    errorhandler = require('errorhandler'),
+    morgan = require('morgan');
 
 var server = express();
 
-var issues;
-
-function updateIssues() {
-  gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=open").then(function (opens) {
-    gh.request("GET /repos/:owner/:repo/issues?per_page=100&state=closed").then(function(closed) {
-      var new_issues = closed.concat(opens);
-      new_issues.sort(function(a,b){
-        return a.number - b.number;
-      });
-      issues = new_issues;
-      console.log("Updated with", issues.length + " github objects");
-    });
-  });
-}
-
-setInterval(updateIssues, 3610000); // update every hour or so
-updateIssues();
+server.use(morgan('combined'));
 
 // Configuration
 server.use(function (req, res, next) {
   res.set("Access-Control-Allow-Origin", "*");
+  res.set("Server", "https://github.com/w3c/hanuman");
   next();
 });
 server.use(compression());
 
 
 server.get('/issues', function (req, res, next) {
-    res.jsonp(issues);
+    res.jsonp(issues.get());
 });
 
-function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.render('error', { error: err });
-}
+server.use(errorhandler);
 
-server.use(errorHandler);
 
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
-    console.log("Express server listening on port %d in %s mode", port, server.settings.env);
+    console.log("Express server listening on port %d", port);
 });
